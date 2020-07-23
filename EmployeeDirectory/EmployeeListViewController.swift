b47c79dcd1c6a354ee4d3602a9c43adb4679793b
David Wright <davidtwright89@gmail.com>
2020-07-23 02:08:25 -0400

    Refactored the code to use the picture and pictureThumbnail attributes

--- EmployeeDirectory/EmployeeListViewController.swift
+++ EmployeeDirectory/EmployeeListViewController.swift
@@ -139,7 +139,7 @@ extension EmployeeListViewController {
     cell.emailLabel.text = employee.email
     cell.phoneNumberLabel.text = employee.phone
     
-    cell.pictureImageView.image = UIImage(data: employee.picture!)
+    cell.pictureImageView.image = UIImage(data: employee.pictureThumbnail!)
     
     return cell
   }
